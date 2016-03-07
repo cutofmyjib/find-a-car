05bd7d16890e341818231bb8379816210b294d2d
cutofmyjib <thedianalozano@gmail.com>
2016-03-07 00:03:30 -0800

    add function to input

--- react-starter-master/app/cityform.js
+++ react-starter-master/app/cityform.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
 
 export default class CityForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { city: '' }
+  }
+
+  getString(e) {
+    var cityStr = e.target.value;
+    this.setState({ city: cityStr })
+  }
+
   render() {
+    console.log(this.state.city)
     return (
       <div>
         <form className="ui form">
           <div className="field">
-            <input type="text" name="city" placeholder="City" />
+            <input type="text" name="city" placeholder="San Francisco" onChange={this.getString.bind(this)}/>
           </div>
         </form>
       </div>
