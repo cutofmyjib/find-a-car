052e2065bbe8adf8f6399f006b6c7ca804298421
cutofmyjib <thedianalozano@gmail.com>
2016-03-11 22:09:14 -0800

    change to div

--- react-starter-master/app/cityform.js
+++ react-starter-master/app/cityform.js
@@ -3,12 +3,8 @@ import React, {Component} from 'react';
 export default class CityForm extends Component {
   render() {
     return (
-      <div>
-        <form className="ui form">
-          <div className="field">
-            <input type="text" name="city" placeholder="San Francisco" onChange={this.props.onChange} />
-          </div>
-        </form>
+      <div className="field">
+        <input type="text" name="city" placeholder="San Francisco" onChange={this.props.onChange} />
       </div>
     );
   }
