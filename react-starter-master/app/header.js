20f5958dcdbde0ec5ab6d6526ed492ffc4d8c626
cutofmyjib <thedianalozano@gmail.com>
2016-03-18 12:45:50 -0700

    link to home

--- react-starter-master/app/header.js
+++ react-starter-master/app/header.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-
+import { Link } from 'react-router'
 export default class Header extends Component {
   render() {
     return (
       <header className="customheader">
-        <h1>FINDA CAR</h1>
+        <Link to='/'><h1>FINDA CAR</h1></Link>
       </header>
     );
   }
